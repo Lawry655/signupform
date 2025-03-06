fe04979b7eb2a7ef7332a8b2c92b5e713f6e3a5d
Lawry655 <lawrenceujah65531844@gmail.com>
2025-03-06 10:35:28 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+function validateAndstoredata(){
+const name = document.getElementById('name').Value;
+const email = document.getElementById('email').Value;
+const phonenumber = document.getElementById('phonenumber').Value;
+const password = document.getElementById('password').Value;
+const confirmpassword = document.getElementById('confirmpassword').Value;
+
+// simple validation //
+if  (name === '' || email === '' ||  password === '' || confirmpassword === ''){
+    alert('Please fill in all fields');
+    return;
+}
+
+// Store data in all local storage 
+    localStorage.setItem('name', name);
+    localStorage.setItem("email", email);
+    localStorage.setItem("phonenumber", phonenumber);
+    localStorage.setItem("password", password);
+    localStorage.setItem("confirmpassword", confirmpassword);
+
+}
+document.getElementById('submit')
+addEventListener('click' ,storeuserdata);{
+document.getElementById('submitted')
+addEventListener('click',loaduserdata)
+}
